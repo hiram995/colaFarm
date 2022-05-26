2b18b91155057bcc257529ffca68b9edcb4a67c9
standandcxs <916194949@qq.com>
2022-05-27 02:30:25 +0800

    first

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,11 +8,15 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Web3 from 'web3'
+let web3 = new Web3()
 
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+
+}
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')  //代码块一
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {   //代码块二
+    resolve: {
+      fallback: {
+        fs: false,
+        crypto: require.resolve("crypto-browserify")
+      }
+    },
+    plugins: [new NodePolyfillPlugin()],
+  },
 })
